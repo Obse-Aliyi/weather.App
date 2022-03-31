7ca54576a456287ce0b766c42af848cf0277f4f0
bint-muktar <obsehassen2018@gmail.com>
2022-03-30 18:36:22 -0600

    Removed HTML code and added forecast to JS

--- src/app.js
+++ src/app.js
@@ -14,6 +14,32 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`
 }
 
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+    let days = ["Wed", "Thur","Fri", "Sat", "Sun", "Mon"];
+    let forecastHTML =`<div class="row">`;
+    days.forEach(function(day){
+        forecastHTML = forecastHTML + 
+        `           <div class="col-2">
+                              <div class="weather-forecast-date">
+                                  ${day}
+                             </div>
+                              <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="36" />
+                              <div class="weather-forecast-temperature">
+    
+                                <span class="weather-forecast-temperature-max"> 18° </span>
+                                 <span class="weather-forecast-temperature-min">12°</span>
+                              </div>
+                          </div>
+                   
+                      `;
+    });
+                   forecastHTML = forecastHTML + `</div>`;
+                  forecastElement.innerHTML = forecastHTML;
+}
+
+
+
 function displayTemperature(response){
     console.log(response.data.main.temp);
     document.querySelector("#temperature").innerHTML =Math.round (celsiusTmeperature);
@@ -48,6 +74,7 @@ function handleSubmit(event){
     search(cityInputElement.value);
 
 }
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit",handleSubmit);
